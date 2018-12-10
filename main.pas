f15f7e45f8f3ac4b46848a7e5f7a3ca607bc5aaa
EquinetPaul <45755573+EquinetPaul@users.noreply.github.com>
2018-12-10 10:54:07 +0100

    Update main.pas

--- main.pas
+++ main.pas
@@ -1,1 +1,22 @@
+(*
+ ------------------------------------------------------------------------------------
+ -- Fichier           : main.pas
+ -- Auteur            : toto noName <toto.noName@eisti.eu>
+ -- Date de creation  : Mon Dec 10 10:52:49 2018
+ --
+ -- But               : But
+ -- Remarques         : Aucune
+ -- Compilation       : fpc
+ -- Edition des liens : fpc
+ -- Execution         : shell
+ ------------------------------------------------------------------------------------
+ *)
+PROGRAM Main;
+(*Début du programme principal*)
+VAR
+(*Déclaration variables utilises dans le programme principal*)
 
+BEGIN
+
+END.
+(*Fin du programme principal*)
