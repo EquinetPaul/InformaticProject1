882602a24f4da0195cc083bab4d3a3a779967d67
EquinetPaul <45755573+EquinetPaul@users.noreply.github.com>
2018-12-11 17:36:27 +0100

    Update cmd.pas

--- cmd.pas
+++ cmd.pas
@@ -1,14 +1,27 @@
 UNIT cmd;
+
 INTERFACE
-(* Déclaration des librairies utilisées *)
 
 (* Déclaration des fonctions utilisables par le programme principal *)
-PROCEDURE afficherCommandes;
+PROCEDURE afficherCommandes();
+PROCEDURE traiterCommande(commande : Char; val : Integer);
 
 IMPLEMENTATION
-(* Déclaration des constantes utilisées *)
 
-PROCEDURE afficherCommandes;
+USES user;
+
+(*
+ ------------------------------------------------------------------------------------
+ -- PROCEDURE         : afficherCommandes
+ -- Auteur            :
+ -- Date de creation  : Tue Dec 11 14:35:05 2018
+ --
+ -- But               : afficher les commande disponibles
+ -- Remarques         : fichier cmd.txt nécessaire
+ -- Pré conditions    : aucune
+ -- Post conditions   : affiche dans le shell les commandes lues dans le fichier
+ ------------------------------------------------------------------------------------*)
+PROCEDURE afficherCommandes();
 VAR
    ligne : String; fic   : Text;
 BEGIN
@@ -22,5 +35,25 @@ BEGIN
    END;
    close(fic);
 END; { afficherCommandes }
+
+(*
+ ------------------------------------------------------------------------------------
+ -- PROCEDURE         : traiterCommande()
+ -- Auteur            : 
+ -- Date de creation  : Tue Dec 11 15:26:45 2018
+ --
+ -- But               : traiter les commandes mises en paramètres lors du lancement du programme
+ -- Remarques         : Aucune
+ -- Pré conditions    : un caractère (désignant la commande) et un entier (si la commande est associée à une valeur) 
+ -- Post conditions   : execute la commande voulu
+ ------------------------------------------------------------------------------------*)
+PROCEDURE traiterCommande(commande : Char ; val : Integer);
+BEGIN
+   CASE commande OF
+     'a' : creeMotAleatoireUtilisateur(val);
+     ELSE
+        afficherCommandes();
+   END;
+END;
 
 END.
