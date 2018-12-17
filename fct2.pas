d9711c7bd43c7c34d467d086b6cc108e820f71a3
EquinetPaul <45755573+EquinetPaul@users.noreply.github.com>
2018-12-17 09:54:23 +0100

    Update fct2.pas
    fonction fonctionnelle

--- fct2.pas
+++ fct2.pas
@@ -1,11 +1,15 @@
 UNIT fct2;
 INTERFACE
-(* Déclaration des librairies utilisées *)
+
+{$mode objfpc}{$H+}
+{$codepage UTF8}
+{$I-}
 
 (* Déclaration des fonctions utilisables par le programme principal *)
 FUNCTION creeMotAleatoire(taille : Integer):WideString ;
 
 IMPLEMENTATION
+USES cwstring, sysutils, crt;
 (* Déclaration des constantes utilisées *)
 CONST
    alphabet: WideString =  'abcdefghijklmnopqrstuvwxyzàâéèêëîïôùûüÿæœç-';
@@ -25,6 +29,7 @@ FUNCTION creeMotAleatoire(taille : Integer):WideString ;
 VAR
    res : WideString; i : Integer;
 BEGIN
+   Randomize;
    res := '';
    FOR i:= 1 TO taille DO BEGIN
       res := res + alphabet[Random(length(alphabet))];
